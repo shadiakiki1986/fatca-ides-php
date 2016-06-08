d138a158dc980560b5b90dc6d5d68c4793188864
shadiakiki1986 <shadiakiki1986@gmail.com>
2016-06-08 16:55:04 +0300

    tabbing [ci-skip]

--- tests/FatcaIdesPhp/AesManagerTest.php
+++ tests/FatcaIdesPhp/AesManagerTest.php
@@ -5,10 +5,10 @@ namespace FatcaIdesPhp;
 class AesManagerTest extends \PHPUnit_Framework_TestCase {
 
     public function testNoDuplicates() {
-	$gm=new AesManager();
-	$x="something";
-	$y=$gm->decrypt($gm->encrypt($x));
-	$this->assertTrue($y==$x);
+      $gm=new AesManager();
+      $x="something";
+      $y=$gm->decrypt($gm->encrypt($x));
+      $this->assertTrue($y==$x);
     }
 
 }
