1d83b91d1931589e47972a66b31e5211b190d9b6
shadiakiki1986 <shadiakiki1986@gmail.com>
2016-06-12 13:00:53 +0000

    add namespace with sed

--- updateXsd.php
+++ updateXsd.php
@@ -16,8 +16,9 @@ use com\mikebevz\xsd2php\LegkoXML;
 $legko = new LegkoXML();
 $schema = $fnH."/FATCA XML Schema v1.1/FatcaXML_v1.1.xsd";
 assert(file_exists($schema));
-$dest = "src/FatcaXsdPhp/";
+$dest = "src/FatcaXsdPhp";
 assert(is_dir($dest));
 $legko->compileSchema($schema, $dest);
 
+exec("sed -i 's/<?php/<?php namespace FatcaXsdPhp;/g' $dest/*php"); // $dest/oecd/ties/isofatcatypes/v1/*php $dest/oecd/ties/stf/v4/*php $dest/oecd/ties/stffatcatypes/v1/*php");
 echo "Done\n";
