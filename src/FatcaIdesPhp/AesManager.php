a89920efc2e22d67c7649f3de467480d2e96cd57
Shadi Akiki <shadiakiki1986@gmail.com>
2016-06-27 16:11:20 +0300

    re-add ability to do ECB and/or CBC

--- src/FatcaIdesPhp/AesManager.php
+++ src/FatcaIdesPhp/AesManager.php
@@ -25,7 +25,7 @@ class AesManager {
   private $sizeKey;
   private $sizeIv;
 
-	function __construct() {
+	function __construct($algoS="CBC") {
     // some code to play around with openssl random generator
     //    $x=openssl_random_pseudo_bytes(32);
     //    $y=unpack("H*",$x);
@@ -33,11 +33,24 @@ class AesManager {
     //    $y=array_values($y)[0];
     //    $this->assertTrue(pack("H*",$y)==$x);
 
+    assert(in_array($algoS,array("CBC","ECB")));
+
     $this->sizeKey = 32;
 		$this->aeskey = openssl_random_pseudo_bytes($this->sizeKey);
+    $this->algoS=$algoS;
 
-    $this->sizeIv = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
-    // size will be 16
+    switch($algoS) {
+      case "CBC":
+        $this->algoI=MCRYPT_MODE_CBC;
+        break;
+      case "ECB":
+        $this->algoI=MCRYPT_MODE_ECB;
+        break;
+      default:
+        throw new Exception("Unsupported algo '".$algoS."'. Please use CBC or ECB");
+    }
+
+    $this->sizeIv = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, $this->algoI);
     assert($this->sizeIv==16);
     $this->iv = mcrypt_create_iv($this->sizeIv, MCRYPT_RAND);
 	}
@@ -49,18 +62,23 @@ class AesManager {
 
 		// add PCKS7 padding
 		// http://php.net/manual/en/function.mcrypt-encrypt.php#47973
-		$block = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, 'cbc');
+		$block = mcrypt_get_block_size(MCRYPT_RIJNDAEL_128, strtolower($this->algoS));
 		$len = strlen($in);
 		$padding = $block - ($len % $block);
 		$in .= str_repeat(chr($padding),$padding);
 
-		$o = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $this->aeskey, $in, MCRYPT_MODE_CBC, $this->iv);
+		$o = mcrypt_encrypt(
+      MCRYPT_RIJNDAEL_128,
+      $this->aeskey,
+      $in,
+      $this->algoI,
+      $this->iv);
 		return $o;
   }
 
 	function decrypt($in) {
 		//http://stackoverflow.com/a/20016774/4126114
-		$dectext = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $this->aeskey, $in, MCRYPT_MODE_CBC, $this->iv);
+		$dectext = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $this->aeskey, $in, $this->algoI, $this->iv);
 		// http://stackoverflow.com/a/7324793/4126114
 		$len = strlen($dectext);
 		$pad = ord($dectext[$len-1]);
@@ -70,15 +88,27 @@ class AesManager {
 	}
 
   function setAesIv($concatenated) {
-    // split aeskey entry into aeskey + iv
-    // Reference: https://www.irs.gov/businesses/corporations/fatca-ides-technical-faqs#EncryptionE21
-    // http://stackoverflow.com/a/1289114/4126114
-    $s1 = strlen($concatenated);
-    $s2 = $this->sizeIv+$this->sizeKey;
-		if($s1!=$s2) throw new \Exception("Invalid aes key + iv concatenation size ".$s1." should be ".$s2);
+    switch($this->algoS) {
+      case "CBC":
+        // split aeskey entry into aeskey + iv
+        // Reference: https://www.irs.gov/businesses/corporations/fatca-ides-technical-faqs#EncryptionE21
+        // http://stackoverflow.com/a/1289114/4126114
+        $s1 = strlen($concatenated);
+        $s2 = $this->sizeIv+$this->sizeKey;
+        if($s1!=$s2) throw new \Exception("Invalid aes key + iv concatenation size ".$s1." should be ".$s2);
 
-    $this->aeskey = substr($concatenated,0,$this->sizeKey);
-    $this->iv = substr($concatenated,$this->sizeKey,$this->sizeIv);
+        $this->aeskey = substr($concatenated,0,$this->sizeKey);
+        $this->iv = substr($concatenated,$this->sizeKey,$this->sizeIv);
+        break;
+
+      case "ECB":
+        // in this case, this is not really concatenated
+        $s1 = strlen($concatenated);
+        $s2 = $this->sizeKey;
+        if($s1!=$s2) throw new \Exception("Invalid aes key size ".$s1." should be ".$s2);
+        $this->aeskey = $concatenated;
+        break;
+    }
   }
 
   function getAesIv() {
