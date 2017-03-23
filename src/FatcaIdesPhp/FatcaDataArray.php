11e54b4a7a5ca9e0bf0c572ebc814e5d89df16e7
Shadi Akiki <shadiakiki1986@gmail.com>
2017-03-23 16:58:59 +0200

    FatcaDataArray revamp: - use type hinting - as the use of this class as a FatcaDataInterface is deprecated, removing the *implements* and removing all the deprecated code

--- src/FatcaIdesPhp/FatcaDataArray.php
+++ src/FatcaIdesPhp/FatcaDataArray.php
@@ -3,7 +3,7 @@
 namespace FatcaIdesPhp;
 
 // Implementation of FatcaDataInterface that is suitable for institutions that have only a flat array of individuals with reportable accounts
-class FatcaDataArray implements FatcaDataInterface {
+class FatcaDataArray {
 
 	var $data; // php data with fatca information
 	var $ts;
@@ -20,9 +20,8 @@ class FatcaDataArray implements FatcaDataInterface {
    * guidMan: instance of GuidManager
    */
 
-	function __construct($dd,$isTest,$corrDocRefId,$taxYear,$conMan,$guidMan=null) {
+	function __construct($dd, $isTest, $corrDocRefId, $taxYear, ConfigManager $conMan, GuidManager $guidMan=null) {
 
-    assert($conMan instanceOf ConfigManager);
     $this->conMan=$conMan;
 
 		$this->data=$dd;
@@ -38,12 +37,14 @@ class FatcaDataArray implements FatcaDataInterface {
       $this->guidManager=new GuidManager(
         $this->conMan->config["ffaid"].".");
     } else {
-      assert($guidMan instanceOf GuidManager);
       $this->guidManager=$guidMan;
     }
   }
 
-  function start() {
+  // This is more of a cleaning function
+  // It could have been part of the constructor
+  // but the purpose was that the constructor do nothing
+  public function start() {
     $this->conMan->check();
 
 		$this->docType=sprintf("FATCA%s%s",$this->isTest?"1":"",$this->corrDocRefId?"2":"1");
@@ -75,181 +76,6 @@ class FatcaDataArray implements FatcaDataInterface {
     //    $this->assertTrue($tz=="UTC");
 		$this->ts3=strftime("%Y-%m-%dT%H:%M:%S", $this->ts); 
 	}
-
-  function filterIndividuals() {
-		$o = array_filter(
-      $this->data,
-      function($x) {
-        return $x["ENT_TYPE"]=="Individual";
-      });
-    if(count($o)<count($this->data)) {
-      trigger_error("FatcaDataArray is not suitable for entries that have type!=Individual. Filtering and dropping ".(count($this->data)-count($o))." entries. Please use FatcaDataOecd instead", E_USER_WARNING);
-    }
-    return $o;
-  }
-
-	function toHtml() {
-		$dv=$this->filterIndividuals();
-		$dv=array_values($dv);
-    $headers=array(
-      "ENT_TYPE","ENT_LASTNAME","ENT_FIRSTNAME","ENT_FATCA_ID","ENT_ADDRESS",
-      "ResidenceCountry","ENT_COD","posCur","Compte","cur","dvdCur","intCur");
-
-    // assert that there no keys more than those defined above
-    array_map(function($x) use($headers) {
-      $ad=array_diff(array_keys($x),$headers);
-      assert(count($ad)==0,"Test no extra headers failed. Found: ".implode(", ",$ad));
-    }, $dv);
-
-	  $th=implode(array_map(function($x) { return "<th>".$x."</th>"; },$headers));
-    $body=array();
-    foreach($dv as $y) {
-      $row=array();
-      foreach($headers as $x1) {
-        if(array_key_exists($x1,$y)) {
-          $x2=$y[$x1];
-          if(!is_array($x2)) {
-            array_push($row, "<td>".$x2."</td>");
-          } else {
-            die("arrays here No longer supported");
-          }
-        } else {
-          array_push($row,"<td>&nbsp;</td>");
-        }
-      }
-      $row=implode($row);
-      array_push($body, "<tr>".$row."</tr>");
-    }
-    $body=implode($body);
-
-		$html2 = sprintf("<table border=1>%s%s</table>\n",$th,$body);
-
-    // pretty print html
-    $doc = new \DOMDocument();
-    $doc->preserveWhiteSpace = true;
-    $doc->formatOutput = true;//false;
-    $doc->loadHTML($html2);
-    $html3=$doc->saveHTML();
-    return $html3;
-	}
-
-	function toXml($utf8=false) {
-  		$di=$this->filterIndividuals();
-
-	    # convert to xml 
-	    #        xsi:schemaLocation='urn:oecd:ties:fatca:v1 FatcaXML_v1.1.xsd'
-	    $diXml=sprintf("
-		<ftc:FATCA_OECD version='1.1'
-		    xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
-		    xmlns:sfa='urn:oecd:ties:stffatcatypes:v2'
-		    xmlns:ftc='urn:oecd:ties:fatca:v2'>
-		    <ftc:MessageSpec>
-			<sfa:SendingCompanyIN>".$this->conMan->config["ffaid"]."</sfa:SendingCompanyIN>
-			<sfa:TransmittingCountry>LB</sfa:TransmittingCountry>
-			<sfa:ReceivingCountry>US</sfa:ReceivingCountry>
-			<sfa:MessageType>FATCA</sfa:MessageType>
-			<sfa:Warning/>
-			<sfa:MessageRefId>%s</sfa:MessageRefId>
-			<sfa:ReportingPeriod>".sprintf("%s-12-31",$this->taxYear)."</sfa:ReportingPeriod>
-			<sfa:Timestamp>$this->ts3</sfa:Timestamp>
-		    </ftc:MessageSpec>
-		    <ftc:FATCA>
-			<ftc:ReportingFI>
-			    <sfa:Name>FFA Private Bank</sfa:Name>
-			    <sfa:Address>
-				<sfa:CountryCode>LB</sfa:CountryCode>
-				<sfa:AddressFree>Foch street</sfa:AddressFree>
-			    </sfa:Address>
-			    <ftc:DocSpec>
-				<ftc:DocTypeIndic>%s</ftc:DocTypeIndic>
-				<ftc:DocRefId>%s</ftc:DocRefId>
-				%s
-			    </ftc:DocSpec>
-			</ftc:ReportingFI>
-		    <ftc:ReportingGroup>
-		    %s
-		    </ftc:ReportingGroup>
-		    </ftc:FATCA>
-		</ftc:FATCA_OECD>",
-		$this->guidManager->get(),
-		$this->docType, // not sure about this versus the same entry below
-    // based on http://www.irs.gov/Businesses/Corporations/FATCA-XML-Schemas-Best-Practices-for-Form-8966-DocRefID
-    // as of June 2016, I can no longer just do this: 		$this->guidManager->guidPrepd[2],
-		$this->guidManager->get(), 
-		!$this->corrDocRefId?"":sprintf("<ftc:CorrDocRefId>%s</ftc:CorrDocRefId>",$this->corrDocRefId), 
-		implode(array_map(
-		    function($x) {
-			// TIN default  issuedBy='US'
-			return sprintf("
-			    <ftc:AccountReport>
-			    <ftc:DocSpec>
-			    <ftc:DocTypeIndic>%s</ftc:DocTypeIndic>
-			    <ftc:DocRefId>%s</ftc:DocRefId>
-			    </ftc:DocSpec>
-			    <ftc:AccountNumber>%s</ftc:AccountNumber>
-			    <ftc:AccountHolder>
-			    <ftc:Individual>
-				<sfa:TIN>%s</sfa:TIN>
-				<sfa:Name>
-				    <sfa:FirstName>%s</sfa:FirstName>
-				    <sfa:LastName>%s</sfa:LastName>
-				</sfa:Name>
-				<sfa:Address>
-				    <sfa:CountryCode>%s</sfa:CountryCode>
-				    <sfa:AddressFree>%s</sfa:AddressFree>
-				</sfa:Address>
-			    </ftc:Individual>
-			    </ftc:AccountHolder>
-			    <ftc:AccountBalance currCode='%s'>%s</ftc:AccountBalance>
-          %s
-          %s
-			    </ftc:AccountReport>
-			",
-			$this->docType, // check the xsd
-      // based on http://www.irs.gov/Businesses/Corporations/FATCA-XML-Schemas-Best-Practices-for-Form-8966-DocRefID
-      // as of June 2016, I can no longer just do this: $this->guidManager->guidPrepd[4],
-			$this->guidManager->get(), 
-			$x['Compte'],
-			$x['ENT_FATCA_ID'],
-			$x['ENT_FIRSTNAME'],
-			$x['ENT_LASTNAME'],
-			$x['ResidenceCountry'],
-			$x['ENT_ADDRESS'],
-			$x['cur'],
-			$x['posCur'],
-      array_key_exists('dvdCur',$x)?sprintf("<ftc:Payment><ftc:Type>FATCA501</ftc:Type><ftc:PaymentAmnt currCode='%s'>%s</ftc:PaymentAmnt></ftc:Payment>",$x['cur'],floor($x['dvdCur'])):"",
-      array_key_exists('intCur',$x)?sprintf("<ftc:Payment><ftc:Type>FATCA502</ftc:Type><ftc:PaymentAmnt currCode='%s'>%s</ftc:PaymentAmnt></ftc:Payment>",$x['cur'],floor($x['intCur'])):""
-			); },
-		    $di
-		),"\n")
-	    );
-
-		// utf8
-		if($utf8) $diXml=utf8_encode($diXml);
-
-		// drop all spaces ... 
-		// This is probably unnecessary, but I had thought that the security threat alert we were receiving was because of this
-		// I later learned that this was not the reason, but I'm leaving it anyway
-		$doc = new \DOMDocument();
-		$doc->preserveWhiteSpace = false;
-		$doc->formatOutput = true;//false;
-		$doc->loadXML($diXml);
-		$diXml=$doc->saveXML();
-
-	    return $diXml;
-	}
-
-  function getIsTest() { return $this->isTest; }
-
-  function getGiinSender() { return $this->conMan->config["ffaid"]; }
-
-  function getTaxYear() {
-    return $this->taxYear;
-  }
-
-  function getTsBase() {
-    return $this->ts;
-  }
 
 
 } // end class
